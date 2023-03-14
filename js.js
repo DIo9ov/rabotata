ae2f84a7e0dae6a3b0012892c9191f6a359fe2e1
DIo9ov <dimidragon9999@gmail.com>
2023-03-14 15:34:17 +0200

    s jekkyl ws gotow

---
+++ js.js
@@ -0,0 +1,18 @@
+let toggle = () => {
+        let element = document.getElementById("tuka");
+        let hidden = element.getAttribute("hidden");
+        if (hidden) {
+          element.removeAttribute("hidden");
+        } else {
+          element.setAttribute("hidden", "hidden");
+        }
+      }
+      let togglee = () => {
+        let element = document.getElementById("tukaa");
+        let hidden = element.getAttribute("hidden");
+        if (hidden) {
+          element.removeAttribute("hidden");
+        } else {
+          element.setAttribute("hidden", "hidden");
+        }
+      }
